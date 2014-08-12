b1e0883fbfdaa7010a8c2c8218eee914a7fee8a6
Ryan <polyrhythm@gmail.com>
2014-08-11 21:21:40 -0700

    yes

--- out/versal_gadget/app.cljs
+++
@@ -1,8 +0,0 @@
-(ns versal-gadget.app
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]))
-
-(enable-console-print!)
-(def player (js/VersalPlayerAPI.))
-
-(.log js/console "hello")
