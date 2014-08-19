de794c88cad53656a625f1cf52afe5840e0ee71b
Ryan <ryan.joshua.jones@gmail.com>
2014-08-18 18:04:17 -0700

    init

--- src/versal-gadget/core.cljs
+++ src/versal-gadget/core.cljs
@@ -8,16 +8,45 @@
 (def app-state
   (atom {}))
 
+; set app-state to reflect what is received from Player
 (defn init []
   (.startListening player)
+  (.setHeight player 300)
   (.on player "attributesChanged"
     (fn [attrs]
-      (.log js/console attrs)
-      (let [cattrs (js->clj attrs)]
-        (swap! app-state cattrs)))))
+      (let [cattrs (js->clj attrs :keywordize-keys true)]
+        (reset! app-state cattrs)))))
 
-(defn click-handler []
-  (.log js/console "you clicked a button"))
+(defn current-hr [app owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:current-hr 110})
+    om/IWillMount
+    (will-mount [_]
+      ; make it life-like
+      (let [change-hr (fn []
+                        (let [rand-integer (rand 1)]
+                          (if (> rand-integer 0.5)
+                            (om/update-state! owner :current-hr inc)
+                            (om/update-state! owner :current-hr dec))))]
+        (.setInterval js/window #(change-hr) 1100)))
+    om/IRenderState
+    (render-state [_ state]
+      (dom/div #js {:className "current-hr"} "Current HR: " (:current-hr state) "bpm"))))
+
+(defn show-hr [hr hr-type]
+  (str hr-type " HR: " hr "bpm"))
+
+(defn hr-view [hr-data owner]
+  (reify
+    om/IRenderState
+    (render-state [_ state]
+      (dom/div #js {:className "hr-view"}
+        (dom/h2 nil "HR View")
+        (dom/div #js {:className "resting-hr"} (show-hr (:rest-hr hr-data) "Resting"))
+        (dom/div #js {:className "max-hr"} (show-hr (:max-hr hr-data) "Max"))
+        (om/build current-hr app)))))
 
 (defn main-view [app owner]
   (reify
@@ -25,11 +54,11 @@
     (render [_]
       (dom/div nil
         (dom/h2 nil "Main view")
-        (dom/button #js {:onClick #(click-handler)} "BUTAN")))))
+        (om/build hr-view (:hr-data app))))))
 
 (defn setup-react []
   (om/root main-view app-state
     {:target (. js/document (querySelector "#app"))}))
 
-(init)
 (setup-react)
+(init)
